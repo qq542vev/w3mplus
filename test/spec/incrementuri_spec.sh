7cfd957453351eada5c3f5ffeaffd8e6c3425ee2
Hajime Fukao <qq542vev@yahoo.co.jp>
2020-07-14 23:35:02 +0900

    test incrementuri

--- test/spec/incrementuri_spec.sh
+++ test/spec/incrementuri_spec.sh
@@ -14,7 +14,7 @@
 ##
 ##   author - qq542vev <https://purl.org/meta/me/>
 ##   version - 1.0.0
-##   date - 2020-06-13
+##   date - 2020-07-14
 ##   since - 2020-06-13
 ##   copyright - Copyright (C) 2020 qq542vev. Some rights reserved.
 ##   license - CC-BY <https://creativecommons.org/licenses/by/4.0/>
@@ -26,11 +26,9 @@
 ##   * Bag report - <https://github.com/qq542vev/w3mplus/issues>
 
 Describe 'Test incrementuri'
-	setup() {
-		command='../../.w3m/w3mplus/bin/incrementuri'
+	incrementuri () {
+		'../../.w3m/w3mplus/bin/incrementuri' ${@+"${@}"}
 	}
-
-  Before 'setup'
 
 	Data
 		#|http://www.example.com/123
@@ -39,30 +37,44 @@ Describe 'Test incrementuri'
 	End
 
 	Example 'Basic increment'
-		When call "${command}" -
+		When call incrementuri -
 		The line 1 of output should equal 'http://www.example.com/124'
 		The line 2 of output should equal 'http://www.example.com/123?key=457'
 		The line 3 of output should equal 'http://www.example.com/123?key=456#790'
 	End
 
-	Example 'Increment +300'
-		When call "${command}" --number '+300' -
+	Example 'Test --number option (increment)'
+		When call incrementuri --number '+300' -
 		The line 1 of output should equal 'http://www.example.com/423'
 		The line 2 of output should equal 'http://www.example.com/123?key=756'
 		The line 3 of output should equal 'http://www.example.com/123?key=456#1089'
 	End
 
-	Example 'Decrement -300'
-		When call "${command}" --number '-300' -
+	Example 'Test --number option (decrement)'
+		When call incrementuri --number '-300' -
 		The line 1 of output should equal 'http://www.example.com/0'
 		The line 2 of output should equal 'http://www.example.com/123?key=156'
 		The line 3 of output should equal 'http://www.example.com/123?key=456#489'
 	End
 
-	Example 'Specify 300'
-		When call "${command}" --number '300' -
+	Example 'Test --number option (specify)'
+		When call incrementuri --number '300' -
 		The line 1 of output should equal 'http://www.example.com/300'
 		The line 2 of output should equal 'http://www.example.com/123?key=300'
 		The line 3 of output should equal 'http://www.example.com/123?key=456#300'
+	End
+
+	Example 'Test --position option'
+		When call incrementuri --position '+2' --number '50' -
+		The line 1 of output should equal 'http://www.example.com/123'
+		The line 2 of output should equal 'http://www.example.com/123?key=50'
+		The line 3 of output should equal 'http://www.example.com/123?key=50#789'
+	End
+
+	Example 'Test --position option'
+		When call incrementuri --position '-2' --number '50' -
+		The line 1 of output should equal 'http://www.example.com/123'
+		The line 2 of output should equal 'http://www.example.com/50?key=456'
+		The line 3 of output should equal 'http://www.example.com/123?key=50#789'
 	End
 End
