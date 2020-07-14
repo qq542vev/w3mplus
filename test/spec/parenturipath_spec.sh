b64ded3fc67474d92137acd050278d6af9225568
Hajime Fukao <qq542vev@yahoo.co.jp>
2020-07-15 00:08:12 +0900

    test parenturipath

--- test/spec/parenturipath_spec.sh
+++ test/spec/parenturipath_spec.sh
@@ -14,7 +14,7 @@
 ##
 ##   author - qq542vev <https://purl.org/meta/me/>
 ##   version - 1.0.0
-##   date - 2020-06-24
+##   date - 2020-07-14
 ##   since - 2020-06-11
 ##   copyright - Copyright (C) 2019-2020 qq542vev. Some rights reserved.
 ##   license - CC-BY <https://creativecommons.org/licenses/by/4.0/>
@@ -26,51 +26,43 @@
 ##   * Bag report - <https://github.com/qq542vev/w3mplus/issues>
 
 Describe 'Test parenturipath'
-	setup() {
-		command='../../.w3m/w3mplus/bin/parenturipath'
+	parenturipath () {
+		'../../.w3m/w3mplus/bin/parenturipath' ${@+"${@}"}
 	}
 
-	Before 'setup'
-
-	Data
-		#|http://www.example.com/a/b/c/
-	End
+	Data 'http://www.example.com/a/b/c/'
 
 	Example 'URI'
-		When call "${command}" -
+		When call parenturipath -
 		The output should equal 'http://www.example.com/a/b/'
 	End
 
-	Data
-		#|http://www.example.com/a/b/c/d
-	End
+	Data 'http://www.example.com/a/b/c/d'
 
 	Example 'URI'
-		When call "${command}" -
+		When call parenturipath -
 		The output should equal 'http://www.example.com/a/b/c/'
 	End
 
 	Example '2 up'
-		When call "${command}" --number 2 -
+		When call parenturipath --number 2 -
 		The output should equal 'http://www.example.com/a/b/'
 	End
 
 	Example '5 up'
-		When call "${command}" --number 5 -
+		When call parenturipath --number 5 -
 		The output should equal 'http://www.example.com/'
 	End
 
-	Data
-		#|file:a/b/c
-	End
+	Data 'file:a/b/c'
 
 	Example '2 up'
-		When call "${command}" --number 2 -
+		When call parenturipath --number 2 -
 		The output should equal 'file:a/'
 	End
 
 	Example '5 up'
-		When call "${command}" --number 5 -
+		When call parenturipath --number 5 -
 		The output should equal 'file:a/'
 	End
 
@@ -80,7 +72,7 @@ Describe 'Test parenturipath'
 	End
 
 	Example '4 up'
-		When call "${command}" --number 4 -
+		When call parenturipath --number 4 -
 		The line 1 of output should equal 'http://www.example.com/'
 		The line 2 of output should equal 'file:a/b/'
 	End
