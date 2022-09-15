759fbd404cc3b2836b3407802b90f26231a1de06
qq542vev <qq542vev@yahoo.co.jp>
2022-09-15 16:49:57 +0900

    Update references

--- documents/references/search/keywords/007300610066.js
+++ documents/references/search/keywords/007300610066.js
@@ -1,1 +1,1 @@
-﻿NDSearch.OnPrefixDataLoaded("saf",["Function"],[["safe_string",,[[,,"AWK",,0,"File:.w3mplus/lib/safe_string.awk:safe_string"],[,,"ShellScript",,0,"File:.w3mplus/lib/safe_string.sh:safe_string"]]]]);
+﻿NDSearch.OnPrefixDataLoaded("saf",["Function","File"],[["safe_string",,[[,,"AWK",,0,"File:.w3mplus/lib/safe_string.awk:safe_string"],[,,"ShellScript",,0,"File:.w3mplus/lib/safe_string.sh:safe_string"],[,"safe_string.awk",,,1,"File:.w3mplus/lib/safe_string.awk:safe_string.awk"],[,"safe_string.sh",,,1,"File:.w3mplus/lib/safe_string.sh:safe_string.sh"]]]]);
