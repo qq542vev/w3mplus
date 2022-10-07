ec0029af552f2399e949436a3c29c5d1db2f25dd
qq542vev <qq542vev@yahoo.co.jp>
2022-10-07 10:10:41 +0900

    Update option_error.sh

--- source/.w3mplus/lib/option_error.sh
+++ source/.w3mplus/lib/option_error.sh
@@ -15,7 +15,7 @@
 ##   id - e7ead215-bfcb-4727-ace1-9fc6d99b9479
 ##   author - <qq542vev at https://purl.org/meta/me/>
 ##   version - 1.0.0
-##   date - 2022-09-02
+##   date - 2022-10-06
 ##   since - 2022-07-26
 ##   copyright - Copyright (C) 2022-2022 qq542vev. Some rights reserved.
 ##   license - <CC-BY at https://creativecommons.org/licenses/by/4.0/>
@@ -40,7 +40,7 @@
 
 option_error() {
 	printf '%s: %s\n' "${0##*/}" "${1}" >&2
-	printf '%s\n' "詳細については '${0##*/} --help' を実行してください。" >&2
+	printf "詳細については '%s' を実行してください。\\n" "${0##*/} --help" >&2
 
 	end_call "${EX_USAGE}"
 }
